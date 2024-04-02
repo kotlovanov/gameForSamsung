c642fee09ef57d1c937c3932a5d3909b836edfa9
kotlovanov <kotlovanovna@gmail.com>
2024-04-02 22:12:02 +0500

    add bigmonster

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.Math.abs;
 
@@ -10,6 +8,7 @@ public class Main {
 
     public static String[][] board;
     public static HashMap<String, String> dictionary;
+    public static List<String> givenList = Arrays.asList("В_негрет", "Тро_ейбус", "Здра_ствуйте", "Вы_грать", "Параш_т", "Ж_ри", "Рю_зак", "Ай_берг", "Ветре_ый", "Стекля_ый");
     public static int step = 0;
     public static int personX, personY;
 
@@ -23,41 +22,54 @@ public class Main {
     }
 
     public static boolean giveTask() {
-        return true;
+
+        int a = random.nextInt(10);
+        int b = random.nextInt(10);
+        System.out.println("Решите пример" + a + " + " + b + " = ");
+        int answer = scanner.nextInt();
+        if (answer == (a + b)) return true;
+        else return false;
+    }
+
+    public static boolean giveBigTask() {
+        String randomElement = givenList.get(random.nextInt(givenList.size()));
+        System.out.println(randomElement);
+//        String answer = scanner.nextLine();
+        String ans = scanner.nextLine();
+        ans = dictionary.get(ans);
+        if (ans.equals(randomElement)) return true;
+        else return false;
     }
 
     public static void main(String[] args) {
         int size = 5;
-        int a = 9;
         personX = 0;
         personY = 4;
         int personLive = 3;
+        dictionary = new HashMap<>();
+        dictionary.put("И", "В_негрет");
+        dictionary.put("ЛЛ", "Тро_ейбус");
+        dictionary.put("В", "Здра_ствуйте");
+        dictionary.put("Ю", " Параш_т");
+        dictionary.put("К", "Рю_зак");
+        dictionary.put("С", " Ай_берг");
+        dictionary.put("Н", "Ветре_ый");
+        dictionary.put("НН", "Стекля_ый");
         int count_monster = size * size - size - 1;
+        int count_bigmonster = 4;
         random = new Random();
         int castleX = 1 + random.nextInt(size - 1);
         int castleY = 0;
-        String person = "Г ";
-        String monster = "Мм";
+        String person = "\uD83E\uDDD9";
+        String bigmonster = "\uD83E\uDDDF";
+        String monster = "\uD83E\uDDB9";
         String castle = "\uD83C\uDFF0";
         String leftBlock = " | ";
         String rightBlock = " |";
         String wall = " + —— + —— + —— + —— + —— + ";
 
-//        System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
         scanner = new Scanner(System.in);
-//        String answer = scanner.nextLine();
-//        switch (answer) {
-//            case "ДА": {
-//                break;
-//            }
-//            case "НЕТ": {
-//                System.out.println("Жаль, приходи еще!");
-//                break;
-//            }
-//            default: {
-//                System.out.println("Данные введены некорректно");
-//            }
-//        }
+
         board = new String[size][size];
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
@@ -66,6 +78,9 @@ public class Main {
         }
         for (int i = 0; i <= count_monster; i++) {
             board[random.nextInt(size)][random.nextInt(size)] = monster;
+        }
+        for (int i = 0; i <= count_bigmonster; i++) {
+            board[random.nextInt(size)][random.nextInt(size)] = bigmonster;
         }
 
         while (personLive > 0 && !(castleX == personX && castleY == personY)) {
@@ -97,12 +112,16 @@ public class Main {
                     personLive--;
                     System.out.println("Вы потеряли жизнь");
                 } else System.out.println("Вы победили монстра");
-            } else {
-                System.out.println("Решите задачу.");
-            }
+            } else if (board[y][x].equals(bigmonster)) {
+                if (!giveBigTask()) {
+                    personLive -= 2;
+                    System.out.println("Вы потеряли две жизни");
+                } else System.out.println("Вы победили большого монстра");
 
+            }
         }
         if (personLive != 0) System.out.println("Вы прошли игру!");
         else System.out.println("Вы проиграли");
+
     }
 }
